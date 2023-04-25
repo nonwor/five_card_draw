dc2f988f2a47706624cb5c2c6e0ea56760959f82
nonwor <nonwor@gmail.com>
2023-04-25 15:59:55 -0700

    functions

--- deck.js
+++ deck.js
@@ -1,75 +1,77 @@
 export class Card{
-    constructor(suit, rank){
-        this.suit = suit;
-        this.rank = rank;
+  constructor(suit, rank){
+      this.suit = suit;
+      this.rank = rank;
 
 
-        //Convert suit and rank to image string
-        let endStr;
-        if(parseInt(rank) > 0){
-            endStr = "r"+ rank;
-        } else{
-            endStr = rank;
-        }
-        let imageURL = suit + "-" + endStr +'.svg'; 
-        this.imageLink = imageURL;
-    }
+      //Convert suit and rank to image string
+      let endStr;
+      if(parseInt(rank) > 0){
+          endStr = "r"+ rank;
+      } else{
+          endStr = rank;
+      }
+      let imageURL = suit + "-" + endStr +'.svg'; 
+      this.imageLink = imageURL;
+  }
 
-    get name(){
-        return `${this.rank} of ${this.suit} :${this.imageLink}`;
-    }
-    //This is for tie breaker
-    get value(){
-        if (this.rank == "Jack"){
-            return 10;
-        }
-        if (this.rank == "Queen"){
-            return 11;
-        }
-        if (this.rank == "King"){
-            return 13;
-        }
-        if (this.rank == "Ace"){
-            return 14;
-        }
-        return this.rank;
-    }
+  get name(){
+      return `${this.rank} of ${this.suit} :${this.imageLink}`;
+  }
+  //This is for tie breaker
+  get value(){
+      if (this.rank == "Jack"){
+          return 10;
+      }
+      if (this.rank == "Queen"){
+          return 11;
+      }
+      if (this.rank == "King"){
+          return 13;
+      }
+      if (this.rank == "Ace"){
+          return 14;
+      }
+      return this.rank;
+  }
 }
 
 export class Deck {
-    constructor() {
-      this.cards = [];
-      const suits = ["hearts", "diamonds", "clubs", "spades"];
-      const ranks = [
-        "A",
-        "02",
-        "03",
-        "04",
-        "05",
-        "06",
-        "07",
-        "08",
-        "09",
-        "10",
-        "J",
-        "Q",
-        "K",
-      ];
-      for (let suit of suits) {
-        for (let rank of ranks) {
-          this.cards.push(new Card(suit, rank));
-        }
+  constructor() {
+    this.cards = [];
+    const suits = ["hearts", "diamonds", "clubs", "spades"];
+    const ranks = [
+      "A",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
+    for (let suit of suits) {
+      for (let rank of ranks) {
+        this.cards.push(new Card(suit, rank));
       }
-    }
-  
-    shuffle() {
-      for (let i = this.cards.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
-      }
-    }
-  
-    deal() {
-      return this.cards.pop();
     }
   }
+
+  shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  deal() {
+    return this.cards.pop();
+  }
+}
+
+
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import {Card, Deck} from './deck.js';
 
 let newGame = document.getElementById("new game");
 let debugGame = document.getElementById("debug game");
+// let buttons = document.getElementsByClassName("c_control");
+// console.log(buttons);
 // let C1 = document.getElementById("c1");
 let c_hand = [document.getElementById("c1"), 
                 document.getElementById("c2"),
@@ -23,8 +25,23 @@ let computer_purse = document.getElementById("computer_purse");
 let player_purse = document.getElementById("player_purse");
 let game_pot = document.getElementById("pot");
 
+let comp_func = [document.getElementById("c_bet"),
+                document.getElementById("c_call"),
+                document.getElementById("c_fold")
+                ];
+let player_func = [document.getElementById("p_bet"),
+                document.getElementById("p_call"),
+                document.getElementById("p_fold")
+                ];
+
+
 //class gamestate
 let game;
+let computer;
+let player;
+
+let betAmount = 20;
+
 class gamestate{
     constructor(computerHand, playerHand){
         this.computerHand = computerHand;
@@ -34,7 +51,14 @@ class gamestate{
         this.computerPurse = 1000;
         this.playerPurse = 1000;
         this.round = 0;
-        this.hands = 0; //This will which hand the game is on, there's only 2 hands 1st, 2nd, 0, 1, then the game ends.
+        this.series = 0; 
+        /*
+        0 = deal
+        1 = pre-switch bet
+        2 = switch option
+        3 = post-switch bet
+        4 = done
+        */
     }
     get all(){
         console.log("Computer Hand");
@@ -50,9 +74,23 @@ class gamestate{
     }
 }
 
-//class playerstate
+class playerstate{
+    constructor(deltCards, money){
+        this.hand = deltCards;
+        this.money = money;
+        this.action = 0; //0 = bet, 1 = call, 2 = fold
+        this.won = 0; //0
+    }
+}
 
-//class computerstate
+class computerstate{
+    constructor(deltCards, money){
+        this.hand = deltCards;
+        this.money = money;
+        this.action = 0;
+        this.won = 0;
+    }
+}
 
 //DOM
 
@@ -95,8 +133,54 @@ const render = () =>{
     let elemPot = document.createElement("p");
     elemPot.textContent = "Pot size: " + game.pot;
     game_pot.appendChild(elemPot);
+}
 
+const ante = ()=>{
+    let anteAmount = 10;
 
+    if(player.money < anteAmount){
+        // all in
+        game.pot += player.money;
+        player.money = 0;
+        game.playerPurse = 0;
+    }else{
+        game.pot += 10
+        player.money -= anteAmount;
+        game.playerPurse -= anteAmount;
+    }
+
+    if(computer.money < anteAmount){
+        // all in
+        game.pot += computer.money;
+        computer.money = 0;
+        game.computerPurse;
+    }else{
+        game.pot += 10
+        player.money -= anteAmount;
+        game.computerPurse -=anteAmount;
+    }
+}
+
+const hideComputerFunction = () =>{
+    for(let element of comp_func){
+        console.log({element});
+        element.disabled = true;
+    }
+    for(let element of player_func){
+        element.disabled = false;
+    }
+    //
+}
+
+const hidePlayerFunction = () =>{
+    for(let element of player_func){
+        element.disabled = true;
+    }
+    for(let element of comp_func){
+        console.log({element});
+        element.disabled = false;
+    }
+    game.turn = 1;
 }
 
 // This is where we init a new game, by making a deck 
@@ -115,6 +199,22 @@ const initNewGame = () => {
     console.log(playerHand);
     game = new gamestate(computerhand, playerHand);
     render();
+    computer = new computerstate(computerhand, game.computerPurse);
+    player = new computerstate(playerHand, game.playerPurse);
+    
+    //Game logic starts here
+    ante();
+    render();
+    let whoStart = Math.floor(Math.random() * 2); //0 = person starts, 1 = computer starts
+    console.log("0 = player start", "1 = comp start ", whoStart);
+    if(whoStart === 0){
+        hideComputerFunction();
+        game.turn = 0;
+    } else{
+        hidePlayerFunction();
+        game.turn = 1;
+    }
+    //
 
 }
 
@@ -122,5 +222,44 @@ const getData=()=>{
     game.all;
 }
 
+const bet=()=>{
+    console.log("bet");
+    //
+}
+
+const call=()=>{    
+    console.log("call");
+}
+
+const fold=()=>{
+    console.log("fold");
+    console.log(game.turn);
+    //if the current player fold other player take the pot and add to their purse
+    if(game.turn == 0){
+        //player fold
+        game.computerPurse += game.pot;
+        computer.money += game.pot;
+    } else {
+        game.playerPurse += game.pot;
+        player.money += game.pot;
+    }
+    //Reset pot to zero
+    game.pot = 0;
+    render();
+    ante();
+    ///
+    render();
+
+
+}
+
 newGame.addEventListener("click", initNewGame);
 debugGame.addEventListener("click", getData);
+
+comp_func[0].addEventListener("click", bet);
+comp_func[1].addEventListener("click", call);
+comp_func[2].addEventListener("click", fold);
+
+player_func[0].addEventListener("click", bet);
+player_func[1].addEventListener("click", call);
+player_func[2].addEventListener("click", fold);
