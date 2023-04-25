071e6203d7db2d26d25fa3b77dfa530492094f98
nonwor <nonwor@gmail.com>
2023-04-25 13:22:29 -0700

    main display with cards

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ import {Card, Deck} from './deck.js';
 
 let newGame = document.getElementById("new game");
 let debugGame = document.getElementById("debug game");
-let C1 = document.getElementById("div3");
+// let C1 = document.getElementById("c1");
+let c_hand = [document.getElementById("c1"), 
+                document.getElementById("c2"),
+                document.getElementById("c3"),
+                document.getElementById("c4"),
+                document.getElementById("c5"),
+            ];
+    
+let P1 = document.getElementById("p1");
+let p_hand = [document.getElementById("p1"), 
+                document.getElementById("p2"),
+                document.getElementById("p3"),
+                document.getElementById("p4"),
+                document.getElementById("p5"),
+            ];
+
+let computer_purse = document.getElementById("computer_purse");
+let player_purse = document.getElementById("player_purse");
+let game_pot = document.getElementById("pot");
 
 //class gamestate
 let game;
@@ -15,6 +33,8 @@ class gamestate{
         this.turn = 0; //0 = player, 1 = computer
         this.computerPurse = 1000;
         this.playerPurse = 1000;
+        this.round = 0;
+        this.hands = 0; //This will which hand the game is on, there's only 2 hands 1st, 2nd, 0, 1, then the game ends.
     }
     get all(){
         console.log("Computer Hand");
@@ -25,6 +45,9 @@ class gamestate{
     get getcomputerHand(){
         return this.computerHand;
     }
+    get getplayerHand(){
+        return this.playerHand;
+    }
 }
 
 //class playerstate
@@ -34,23 +57,49 @@ class gamestate{
 //DOM
 
 const render = () =>{
-    let elem = document.createElement("img");
-    let source1 = game.getcomputerHand[0].imageLink;
-    console.log(source1);
-    elem.setAttribute("src","./images/" + source1);
-
-    console.log(C1);
-    C1.appendChild(elem);
-    //get image
-    // let source1 = game.getcomputerHand[0].imageLink;
-    // console.log(source1);
-    // elem.attributes("src","./images" + source1);
-    // C1.appendChild(elem);
     
+    //Render computer hand
+    for(let i = 0; i<5 ;i++){
+        c_hand[i].innerHTML = '';
+        let elem = document.createElement("img");
+        let source1 = game.getcomputerHand[i].imageLink;
+        elem.setAttribute("src","./images/" + source1);
+        c_hand[i].appendChild(elem);
+    }
+    
+    //Render palyer hand
+    for(let i = 0; i<5 ;i++){
+        p_hand[i].innerHTML = '';
+        let elem = document.createElement("img");
+        let source1 = game.getplayerHand[i].imageLink;
+        elem.setAttribute("src","./images/" + source1);
+        p_hand[i].appendChild(elem);
+    }
+
+    // Render Pot
+
+    // Render Player Purse
+    computer_purse.innerHTML = '';
+    let elemCP = document.createElement("p");
+    elemCP.textContent = "Computer Purse: " +game.computerPurse;
+    computer_purse.appendChild(elemCP);
+
+    // render Computer Purse
+    player_purse.innerHTML = '';
+    let elemPP = document.createElement("p");
+    elemPP.textContent = "Computer Purse: " + game.playerPurse;
+    player_purse.appendChild(elemPP);
+
+    // render pot
+    game_pot.innerHTML = '';
+    let elemPot = document.createElement("p");
+    elemPot.textContent = "Pot size: " + game.pot;
+    game_pot.appendChild(elemPot);
+
 
 }
 
-
+// This is where we init a new game, by making a deck 
 const initNewGame = () => {
     console.log("New Game Button Clicked");
     let newDeck = new Deck();
@@ -62,7 +111,6 @@ const initNewGame = () => {
         computerhand.push(newDeck.deal());
         playerHand.push(newDeck.deal());
     }
-
     console.log(computerhand);
     console.log(playerHand);
     game = new gamestate(computerhand, playerHand);
